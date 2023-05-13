3e73fa931c420b97b6bd801f66e5a2e9490008f2
Franco Gonzales <gonzales557.fg@gmail.com>
2023-05-13 15:16:20 -0300

    arreglos para entrega final

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
+ // Definimos una clase Cliente que tiene 2 atributos: nombre, apellido
 
-
-  // Definimos una clase Cliente que tiene 2 atributos: nombre, apellido
-
-  class Cliente {
-    constructor(nombre, apellido) {
-      this.nombre = nombre;
-      this.apellido = apellido;
-      
-  }
-  }
+ class Cliente {
+  constructor(nombre, apellido) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    
+}
+}
 
 //Vamos a crear un array vacío que almacene los datos de los
 //clientes ingresados por medio de un push.
@@ -21,56 +19,56 @@ const form = document.getElementById("formulario");
 //Vamos a trabajar con el formulario, tomar sus datos, crear un objeto y luego almacenamos esos datos en el array vacío
 
 form.addEventListener("submit", (e)=>{
-  //evitamos el comportamiento por default del form
-  e.preventDefault();
+//evitamos el comportamiento por default del form
+e.preventDefault();
 
-  const name = document.getElementById("nombre");
-  const lastname = document.getElementById("apellido");
-  const error = document.getElementById("error");
+const name = document.getElementById("nombre");
+const lastname = document.getElementById("apellido");
+const error = document.getElementById("error");
 
-  if (name.value === "" || lastname.value === "") {
-    error.style.display = "block";
-  } else {
-    //crear un objeto que sea el cliente:
-    const cliente = new Cliente(name.value, lastname.value);
-    arrayClientes.push(cliente);
-    console.log(arrayClientes);
+if (name.value === "" || lastname.value === "") {
+  error.style.display = "block";
+} else {
+  //crear un objeto que sea el cliente:
+  const cliente = new Cliente(name.value, lastname.value);
+  arrayClientes.push(cliente);
+  console.log(arrayClientes);
 
-    //Reseteamo el form al mandar los datos
-    formulario.reset();
+  //Reseteamo el form al mandar los datos
+  formulario.reset();
 
-    // Guardamos los datos en el localstorage
-    localStorage.setItem("clientes", JSON.stringify(arrayClientes));
+  // Guardamos los datos en el localstorage
+  localStorage.setItem("clientes", JSON.stringify(arrayClientes));
 
-    // Permitir la reserva del servicio y ocultar el mensaje de error
-    error.style.display = "none";
-  }
+  // Permitir la reserva del servicio y ocultar el mensaje de error
+  error.style.display = "none";
+}
 });
 
 
 // Definir los servicios de la peluquería como objetos
 const corte = {
-  nombre: 'Corte de cabello',
-  precio: 1000,
-  duracion: 30,
+nombre: 'Corte de cabello',
+precio: 1000,
+duracion: 30,
 };
 
 const tinte = {
-  nombre: 'Color y decoloracion',
-  precio: 1500,
-  duracion: 60,
+nombre: 'Color y decoloracion',
+precio: 1500,
+duracion: 60,
 };
 
 const barba = {
-  nombre: 'Arreglo de barba',
-  precio: 500,
-  duracion: 15,
+nombre: 'Arreglo de barba',
+precio: 500,
+duracion: 15,
 };
 
 const ninos = {
-  nombre: 'Corte niños',
-  precio: 800,
-  duracion: 45,
+nombre: 'Corte niños',
+precio: 800,
+duracion: 45,
 };
 
 // Crear un array para almacenar los servicios disponibles
@@ -79,33 +77,75 @@ const servicios = [corte, tinte, barba, ninos];
 // Variable para almacenar los servicios seleccionados
 let serviciosSeleccionados = [];
 
- // Guardamos los datos en el localstorage
+// Guardamos los datos en el localstorage
 const contenedorServicios = document.getElementById("contenedorServicios");
 let serviciosGuardados = JSON.parse(localStorage.getItem("serviciosSeleccionados")) || [];
 
 // Aplico DOM con el metodo forEach para recorrer el array y generar los div con los servicios
 servicios.forEach(servicio => {
-  const div= document.createElement ("div");
-  div.innerHTML=`<p> Nombre del servicio: ${servicio.nombre}<p>
-                <p> Precio del servicio: ${servicio.precio}<p>
-                <p> Duracion del servicio: ${servicio.duracion}<p>
-                <button onclick="agregarServicio(${servicio.precio}, '${servicio.nombre}')">Reservar Servicio</button>`;
-  contenedorServicios.appendChild(div);
+const div= document.createElement ("div");
+div.innerHTML=`<p> Nombre del servicio: ${servicio.nombre}<p>
+              <p> Precio del servicio: ${servicio.precio}<p>
+              <p> Duracion del servicio: ${servicio.duracion}<p>
+              <button onclick="agregarServicio(${servicio.precio}, '${servicio.nombre}')">Reservar Servicio</button>`;
+contenedorServicios.appendChild(div);
 } )
-  
+
+
+function reservarServicio(servicio) {
+  return new Promise((resolve, reject) => {
+    // Mostrar el diálogo de confirmación utilizando SweetAlert
+    Swal.fire({
+      title: '¿Está seguro que desea reservar el servicio seleccionado?',
+      text: 'Su reserva quedará confirmada luego de aceptar',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Aceptar'
+    }).then((result) => {
+      // Comprobar si el usuario confirmó la reserva
+      if (result.isConfirmed) {
+        // Resolver la promesa con el servicio reservado
+        resolve(servicio);
+      } else {
+        // Rechazar la promesa con un mensaje de cancelación
+        reject('Reserva cancelada');
+      }
+    });
+  });
+}
 
 function agregarServicio(precio, nombre) {
   const servicio = { nombre: nombre, precio: precio };
 
   // Verificar si hay un cliente registrado antes de reservar un servicio
   if (arrayClientes.length === 0) {
-    const mensaje = document.getElementById("mensaje");
-    mensaje.textContent = "Debe ingresar su nombre y apellido antes de reservar un servicio.";
+    const mensaje = document.getElementById('mensaje');
+    mensaje.textContent =
+      'Debe ingresar su nombre y apellido antes de reservar un servicio.';
     return;
   }
 
-  serviciosGuardados.push(servicio);
-  localStorage.setItem("serviciosSeleccionados", JSON.stringify(serviciosGuardados));
-  alert(`El servicio de ${nombre} ha sido reservado y tiene un precio de ${precio} pesos.\n Muchas gracias por su reserva`);
-}
+  // Llamar a la función de reserva y manejar la promesa resultante
+  reservarServicio(servicio)
+    .then(() => {
+      // Si la promesa se resuelve, agregar el servicio original al array de servicios guardados
+      serviciosGuardados.push(servicio);
+      localStorage.setItem(
+        'serviciosSeleccionados',
+        JSON.stringify(serviciosGuardados)
+      );
 
+      // Mostrar un mensaje de confirmación
+      Swal.fire(
+        `El servicio de ${servicio.nombre} ha sido reservado`,
+        'Para confirmar día y horario, enviar sus datos al siguiente WhatsApp: 11 6998-6557',
+        'success'
+      );
+    })
+    .catch((error) => {
+      // Si la promesa es rechazada, mostrar un mensaje de error en la consola
+      console.log(error);
+    });
+}
